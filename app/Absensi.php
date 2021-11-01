36bed08c57b949be1043880e7131486226e381ea
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2021-11-01 23:41:50 +0700

    create controller dan editing model

--- app/Absensi.php
+++ app/Absensi.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Absensi extends Model
 {
-    //
+    protected $table = 'absensi';
+    protected $fillable = ['id', 'nik_pegawai', 'full_name_pegawai'];
 }
--- app/Pegawai.php
+++ app/Pegawai.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pegawai extends Model
 {
-    //
+    protected $table = 'pegawai';
+    protected $fillable = ['id', 'nik', 'full_name', 'email', 'mobile_number', 'address'];
 }
